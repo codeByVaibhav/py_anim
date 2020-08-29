fd1729b3e35871fe3e920a4891b0cc3286e6e9b7
Vaibhav Shrma <k.vaibhav9013@gmail.com>
2020-08-30 00:56:03 +0530

    updated example

--- example.py
+++ example.py
@@ -19,25 +19,36 @@ class StartScene(Scene):
         super().__init__(width=width, height=height, fps=fps)
 
     def begin(self):
-        self.camera.pos = vector(0, 0, 8)
+        self.camera.pos = vector(0, 0, 4)
 
-        text = TexText('Hello World', mat=Material(
-            stroke=DARK_BLUE, stroke_width=4))
-        sphere = Cuboid(1, 1, 1)
+        # text = TexText('Hello World', mat=Material(
+        #     stroke=DARK_BLUE, stroke_width=4))
+        cube = Cuboid(1, 1, 1, mat=Material(
+            stroke=DARK_BLUE, stroke_width=8))
+        circle = Circle(mat=Material(stroke=ORANGE, stroke_width=8))
+        square = Rectangle(mat=Material(stroke=RED_D, stroke_width=8))
+        line = Line(start=VEC3_LEFT*10, end=VEC3_RIGHT*10,
+                    mat=Material(stroke=PURPLE_A, stroke_width=8))
 
         self.render(
-            ShowCreation(text),
-            MorphShape(text, sphere),
+            ShowCreation(cube),
             RotateFrame(
                 VEC3_X_AXIS + VEC3_Z_AXIS, 360,
-                self.get_objs_frame(sphere),
-                speed=0.008
+                self.get_objs_frame(cube),
+                speed=0.05
             ),
         )
 
-        self.add_objs_to_background(sphere)
+        self.add_objs_to_background(cube)
 
-        self.pause(0.3)
+        self.pause(0.2)
+        self.background_frame = []
+
+        self.render(
+            MorphShape(cube, circle),
+            MorphShape(circle, square),
+            MorphShape(square, line)
+        )
 
 
 if __name__ == "__main__":
